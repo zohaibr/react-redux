6e1f8e040545e8b7b8412239234f59b5ed65ec66
Scott Anderson <scottya1992@gmail.com>
2017-02-07 13:07:43 -0500

    completed All Albums, starting View Switching

--- browser/react/main.js
+++ browser/react/main.js
@@ -4,81 +4,35 @@ import ReactDOM from 'react-dom';
 import Albums from './albums.js'
 import axios from 'axios';
 
-const fakeAlbums = [
-  {
-    name: 'Abbey Road',
-    id: 1,
-    imageUrl: 'http://fillmurray.com/300/300',
-    songs: [
-      {
-        id: 1,
-        name: 'Romeo & Juliette',
-        artists: [
-          { name: 'Bill' }
-        ],
-        genre: 'Funk',
-        audioUrl: 'https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3'
-      },
-      {
-        id: 2,
-        name: 'White Rabbit',
-        artists: [
-          { name: 'Bill' },
-          { name: 'Alice' }
-        ],
-        genre: 'Fantasy',
-        audioUrl: 'https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3'
-      },
-      {
-        id: 3,
-        name: 'Lucy in the Sky with Diamonds',
-        artists: [
-          { name: 'Bob' }
-        ],
-        genre: 'Space',
-        audioUrl: 'https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3'
-      }
-    ]
-  },
-  {
-    name: 'Yellow Submarine',
-    id: 2,
-    imageUrl: 'http://fillmurray.com/300/300',
-    songs: [
-      {
-        id: 4,
-        name: 'London Calling',
-        artists: [
-          { name: 'Bill' }
-        ],
-        genre: 'Punk',
-        audioUrl: 'https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3'
-      }
-    ]
-  }
-];
-
 
 export default class Main extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {albums: fakeAlbums};
+    this.state = {albums: []};
   }
   render() {
      return (
       <Albums albums = {this.state.albums}/>
     );
   }
+
+  componentDidMount() {
+    const logError = console.error.bind(console);
+
+    axios.get('api/albums')
+      .then(res => res.data)
+      .then(albumsFromServer => {
+        albumsFromServer = albumsFromServer.map(album => {
+          album.imageUrl = `/api/albums/${album.id}/image`;
+          return album;
+        });
+        this.setState({ albums: albumsFromServer });
+      })
+      .catch(logError);
+    }
 }
 
 
 
-const toJson = response => response.data;
-const log = console.log.bind(console);
-const logError = console.error.bind(console);
 
-axios.get('api/albums')
-  .then(toJson)
-  .then(log)
-  .catch(logError);
 // <div id="main" className="container-fluid"
